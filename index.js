0c00890ac5001e26389bbf8575f7f4b41cb64719
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-17 15:24:03 +0600

    create collection for menu and review and api create for menu and review

--- index.js
+++ index.js
@@ -30,9 +30,15 @@ async function run() {
         // Send a ping to confirm a successful connection
 
         const menuCollection = client.db("bistroRestaurantCollection").collection("menu")
+        const reviewsCollection = client.db("bistroRestaurantCollection").collection("reviews")
 
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get("/reviews", async (req, res) => {
+            const result = await reviewsCollection.find().toArray()
             res.send(result)
         })
 

54612a15f2fde0934711c70f21bddb14217c28c8
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-24 04:50:38 +0600

    create api for admin pannel

--- index.js
+++ index.js
@@ -35,6 +35,50 @@ async function run() {
 
         const cartsCollection = client.db("bistroRestaurantCollection").collection("carts")
 
+        const userCollection = client.db("bistroRestaurantCollection").collection("users")
+
+        // user collection
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "User Already Exists", insertId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get("/users", async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
+        // menu collection
+
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)

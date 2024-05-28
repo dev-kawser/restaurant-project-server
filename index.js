229350012bd1848ae482bc4804273c2c258c8dbd
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-28 17:12:05 +0600

    payment api created

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://api.imgbb.com/1/upload',
+    ]
+}));
 app.use(express.json())
 
 
@@ -93,7 +99,7 @@ async function run() {
 
         // for users
 
-        app.get("/users",verifyToken, verifyAdmin, async (req, res) => {
+        app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
@@ -138,7 +144,27 @@ async function run() {
 
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
-            res.send(result)
+            res.send(result);
+        })
+
+        app.get("/menu/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+            const menuItem = req.body;
+            const result = await menuCollection.insertOne(menuItem);
+            res.send(result);
+        })
+
+        app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
         })
 
         app.get("/reviews", async (req, res) => {
@@ -168,6 +194,25 @@ async function run() {
             const result = await cartsCollection.deleteOne(query);
             res.send(result);
         })
+
+        // PAYMENT INTENT
+
+        app.post('/create-checkout-session', async (req, res) => {
+
+            const {price} = req.body;
+            // console.log("price" , price);
+            const amount = parseInt(price * 100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        });
+
 
 
 
